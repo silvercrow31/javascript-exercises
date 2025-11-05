ae5bc062df8518b4e5b831eb9585a2a795804dfe
silvercrow31 <53498833+silvercrow31@users.noreply.github.com>
2025-11-05 04:34:11 -0300

    Solve exercises 5-7

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,24 @@
-const repeatString = function() {
+const repeatString = function(a,b) {
+    
+    const leString = a;
+    const numOfTimes = b;
 
+    if (b < 0) {
+        return "ERROR"
+    } else if (b == 0) {
+        return ""
+    } else {
+
+        let newString = "";
+        let counter = 0;
+        while (counter < numOfTimes) {
+            counter++;
+            newString = newString + leString;
+        }        
+        return newString
+    }
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(a) {
+    let leString = a;
+    let newString = "";
+
+    while (leString) {
+        newString = newString.concat(leString.at(-1));
+        leString = leString.slice(0,-1);
+    }
+    return newString
 
 };
 

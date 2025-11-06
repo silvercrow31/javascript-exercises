075436ace114a0d9ad8d86b8618fec0e894be0b2
silvercrow31 <53498833+silvercrow31@users.noreply.github.com>
2025-11-05 22:23:58 -0300

    Solve exercises 8 and 9

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(a,...b) {
+    const array = a;
+    const itemsToRemove = b;
+    let newArray= [];
+    array.forEach(myFunction)
+    
+    return newArray
+    
+    function myFunction(value){
+        if (!itemsToRemove.includes(value)) {
+            newArray = newArray.concat(value)
+        }
+    }
+    
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,29 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    
+    // error check before anything
+    if (a < 0 
+        || b < 0
+        || !Number.isInteger(a)
+        || !Number.isInteger(b))
+        {
+        return "ERROR"
+    }
+    // sets variables
+    let alpha = a;
+    let omega = b;
+
+    // checks if first number is higher than the second. defines alpha and omega accordingly
+    if (alpha > omega) {
+        alpha = b; omega = a
+        }
+    
+    // sums the numbers
+    let sum = alpha;
+    for (let myNumber = alpha; myNumber != omega; sum = sum + myNumber) {
+        myNumber++;
+    }
+
+    return sum;
 
 };
 

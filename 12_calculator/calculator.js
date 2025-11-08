9b1598ca58b242e6838c56ffbf32a2564cf8b6b4
silvercrow31 <53498833+silvercrow31@users.noreply.github.com>
2025-11-08 00:22:20 -0300

    Solve exercise 13

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,35 +1,41 @@
 const add = function(firstNumber, secondNumber) {
-	return (firstNumber + secondNumber)
+	return (firstNumber + secondNumber);
 };
 
 const subtract = function(firstNumber, secondNumber) {
-	return (firstNumber - secondNumber)
+	return (firstNumber - secondNumber);
 };
 
 const sum = function(a) {
-  const array = a
-	let sumToReturn = 0
-  for (i = 0; i < array.length; i++) {
-    sumToReturn=sumToReturn + array.at(i)
+  const array = a;
+	let sumToReturn = 0;
+  for (let i = 0; i < array.length; i++) {
+    sumToReturn=sumToReturn + array.at(i);
   }
-  return sumToReturn
+  return sumToReturn;
 };
 
 
 const multiply = function(a) {
-  const array = a
-  let multiplyToReturn = 1
-  for (i = 0; i < array.length; i++) {
-    multiplyToReturn=multiplyToReturn * array.at(i)
-  }  return multiplyToReturn
+  const array = a;
+  let multiplyToReturn = 1;
+
+  for (let i = 0; i < array.length; i++) {
+    multiplyToReturn=multiplyToReturn * array.at(i);
+  }  
+  return multiplyToReturn;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(givenNumber) {
+  let numberToReturn = 1;
+  for(let i = 1; i <= givenNumber; i++) {
+  numberToReturn = numberToReturn * i;
+  }
+  return numberToReturn;
 };
 
 // Do not edit below this line
--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,26 @@
-const palindromes = function () {
+const palindromes = function (givenWord) {
+    let wordProcessed;
+    itemsToRemove = ["-", "_", ",", " ", "\"", "\´", "."];
+    wordLength=givenWord.length;
+    let reversedWordProcessed = "";
+
+    for (let i = 0; i===wordLength; i++) {
+        let currentChar=givenWord.at(i)
+
+        // remove punctuation and word breaks
+        
+        if (itemsToRemove.include(currentChar)) {
+            wordProcessed = givenWord.replace(/currentChar/g,"")      
+        }
+        wordProcessed = wordProcessed.toLowercase()
+        // get the reversed string
+        reversedWordProcessed = reversedWordProcessed.concat(currentChar)
+    }
+    if (reversedWordProcessed === wordProcessed) {
+        return true
+    }   else {
+        return false
+    }
 
 };
 

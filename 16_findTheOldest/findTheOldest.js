7529f7297b23331241209f48bb37894eee072721
silvercrow31 <53498833+silvercrow31@users.noreply.github.com>
2025-11-20 21:15:04 -0300

    Create findTheOldest using forEach iteration

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(a) {
+    const givenArray = a;
+    let oldestAge = 0;
+    let oldestPerson = {};
+    givenArray.forEach(checkIfOldest);
 
+    return oldestPerson;
+
+    // función anidada que recibe personas (object), calcuula su edad,
+    // compara con un valor y actualiza en base a la comparación
+    function checkIfOldest(p) {
+
+            let currentPerson = p;
+            let currentPersonAge = p.yearOfDeath - p.yearOfBirth;
+
+            if (currentPersonAge > oldestAge) {
+                oldestPerson = currentPerson;
+                oldestAge = currentPersonAge;
+            }
+    }
 };
 
 // Do not edit below this line
